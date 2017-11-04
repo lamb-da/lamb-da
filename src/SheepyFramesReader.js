f13cce1e2e76c64a7c4ea119315dbc571872ebe9
Maciek Sakrejda <maciek@heroku.com>
2017-11-03 18:04:58 -0700

    Move from gif to png for easier Heroku support

--- src/SheepyFramesReader.js
+++ src/SheepyFramesReader.js
@@ -9,7 +9,7 @@ SheepyFramesReader.prototype.getFrames = function() {
     let frames = [];
     let numberOfFrames = this.sheepyConfig.getNumberOfFrames();
     for(var i = 1; i <= numberOfFrames; i++) {
-        frames.push(`${config.baseSheepys}/${this.sheepyConfig.getSheepyType()}/frame-${leftpad(i, 3, '0')}.gif`);
+        frames.push(`${config.baseSheepys}/${this.sheepyConfig.getSheepyType()}/frame-${leftpad(i, 3, '0')}.png`);
     }
     return frames;
 }
